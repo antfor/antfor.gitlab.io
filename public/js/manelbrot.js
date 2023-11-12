e281236857f70feb6a586664775bb23dcead8699
antfor <38752136+antfor@users.noreply.github.com>
2023-11-12 07:52:38 +0100

    Mandelbrot with Perturbation theory for better zoooooom!

--- public/js/manelbrot.js
+++ public/js/manelbrot.js
@@ -13,33 +13,43 @@ precision highp float;
 uniform vec2 resolution;
 uniform float time;
 
-vec2 zoom(in vec2 zero){
+float animation_time = 30.0;
 
-    float tot_time = 1.0;//5.0/1000.0;
-    float animation_time = 30.0;
+float zoom(){
 
-    
     float ftime = 0.0;
 
     if(time < animation_time) 
         ftime = 1.0 - pow(cos(time * 3.14159265 / animation_time),8.0);
     
-    
-    vec2 pos = vec2(-1.49,0.0);
-
     float zoom_min = 1.3;
     float zoom_max = 1.0/10000.0;
 
-    float zoom = zoom_max * ftime/tot_time + zoom_min * (1.0 - ftime/tot_time);
+    float zoom = zoom_max * ftime + zoom_min * (1.0 - ftime);
 
-    return zoom * zero + pos;
+    return zoom;
+}
+
+vec2 translate(){
+
+    vec2 pos =  vec2(-1.49,0.0);
+    return pos;
+}
+
+float smooth(in float l, in vec2 z){
+
+    if( l < 0.0 ) return 0.0;
+    
+    float sl = 1.0 + l - log(log(length(z))/log(2.0))/log(2.0);
+
+    return sl;
 }
 
 float mandelbrot( in vec2 fragCoord )
 {
  
     vec2 p = (fragCoord.xy*2.0 - resolution.xy) * 2.0/ resolution.x;
-    vec2 c = zoom(p);
+    vec2 c = zoom() * p + translate();
 
    {
     float c2 = dot(c, c);
@@ -53,21 +63,48 @@ float mandelbrot( in vec2 fragCoord )
     if( s2 < 0.0 ) return 0.0;
    }
 
-    float l = 0.0;
+    float l = -1.0;
     vec2 z  = vec2(0.0);
 
     for( int i=0; i< 256; i++ )
     {
         z = vec2( z.x*z.x - z.y*z.y, 2.0*z.x*z.y ) + c;
-        if( dot(z,z)>= 100.0) break;
-        l += 1.0;   
+        if( dot(z,z)>= 100.0) {
+            l = float(i);  
+            break;
+        } 
     }
     
-    if( l>=256.0 ) return 0.0;
-    
-    float sl = 1.0 + l - log(log(length(z))/log(2.0))/log(2.0);
+    return smooth(l,z);
 
-    return sl;
+}
+
+float mandelbrot_prep( in vec2 fragCoord )
+{
+    vec2 p = (fragCoord.xy*2.0 - resolution.xy) * 2.0/ resolution.x;
+
+    vec2 c = translate();
+    vec2 dc = p * zoom();
+
+    vec2 z = vec2(0.0);
+    vec2 dz = vec2(0.0);
+
+    float l = -1.0;
+
+    for(int i=0; i< 256; i++){
+
+        vec2 a = 2.0*z+dz;
+        dz = vec2(a.x*dz.x-a.y*dz.y, a.x*dz.y+a.y*dz.x) + dc;
+        z = vec2( z.x*z.x - z.y*z.y, 2.0*z.x*z.y ) + c;
+
+        if( dot(dz,dz)>= 100.0) {
+            l = float(i);  
+            break;
+        }
+
+    }
+
+    return smooth(l,z + dz); // dz?
 }
 
 void main()
@@ -80,7 +117,8 @@ void main()
     //float l3 = mandelbrot(gl_FragCoord.xy + vec2(-0.125,-0.125));
     //float l = (l0+l1+l2+l3)*0.25;
 
-    float l = mandelbrot(gl_FragCoord.xy);
+    //float l = mandelbrot(gl_FragCoord.xy);
+    float l = mandelbrot_prep(gl_FragCoord.xy);
     
     col = 0.5 + 0.5*cos( 3.0 + l*0.15 + vec3(0.0,0.6,1.0));
     //col =  vec3(l/64.0);
