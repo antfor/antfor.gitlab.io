01e09e6a83bb26cde608f9d86981a0608a73f8aa
antfor <38752136+antfor@users.noreply.github.com>
2023-12-06 22:42:30 +0100

    restructured the code into multiple modules

---
+++ public/js/LsystemModule/Rule.mjs
@@ -0,0 +1,17 @@
+
+import {State} from './State.mjs';
+
+"use strict";
+
+class Rule {
+
+    constructor(pred, succ, func, val = false) {
+      this.pred = pred;
+      this.succ = succ;
+      this.func = func;
+      this.valuFunction = val;
+    }
+  
+}
+
+export {Rule};
