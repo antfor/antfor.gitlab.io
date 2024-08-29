a1834d24b6517017f74ab44a9ee394140831b493
antfor <antfor@student.chalmers.se>
2024-08-29 16:55:44 +0200

    doughnut chart added

---
+++ front/website/src/calculators/intrest/doughnutChart.jsx
@@ -0,0 +1,45 @@
+import { Doughnut } from 'react-chartjs-2';
+import styles from './intrest.module.css';
+import { simplifyValue } from './parse';
+import {
+    Chart as ChartJS,
+    ArcElement,
+    Tooltip,
+  } from 'chart.js';
+
+  ChartJS.register(
+    Tooltip,
+    ArcElement,
+  );
+  
+const options ={
+  plugins: {
+    tooltip: {
+      callbacks: {
+        label: (context) => {return(simplifyValue(context.parsed,2)+'%')}, }
+    }
+  },
+  responsive: true,
+  maintainAspectRatio: false,
+}
+
+export function DoughnutChart(datasets, last, tot){
+
+ 
+    const percent = datasets.map((dataset) => (dataset.data[last]*100.0/tot));
+    const colors = datasets.map((dataset) => dataset.backgroundColor);
+
+    const data ={
+      id: 'doughnut',
+      datasets:[{
+            data: percent,
+            backgroundColor: colors,
+            borderColor: '#2b3035',
+      }]
+    }
+
+    return(<Doughnut data={data}  options={options} className={styles.doughnut}/>);
+
+}
+
+
--- front/website/src/calculators/intrest/resultat.jsx
+++ front/website/src/calculators/intrest/resultat.jsx
@@ -2,6 +2,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { simplifyValue } from './parse.js';
 import styles from './intrest.module.css'; 
+import { DoughnutChart } from './doughnutChart.jsx';
 
 
 
@@ -12,11 +13,12 @@ function getColor(dataset){
 
 function paragraph(dataset, index, tot){
     const label = dataset.label;
-    const data = dataset.data[index];
-    const percent = data/tot*100;
+    const value = dataset.data[index];
+    const data = simplifyValue(value, 0);
+    const percent = simplifyValue(value*100.0/tot, 0);
     return(
         <Col key={label}>
-            <p className={styles.resultat}>{label}: <span style={getColor(dataset)}>{simplifyValue(data,0)} kr ({simplifyValue(percent,0)}%) </span></p>
+            <p className={styles.resultat}>{label}: <span style={getColor(dataset)}>{data} kr ({percent}%) </span></p>
         </Col>
     );
 }
@@ -43,19 +45,25 @@ export default function Result(datasets, total, index){
     const acc = datasets.filter((dataset) => !dataset.intrest);
 
     return(
-        <div>
-        <h2>Resultat</h2>
-            <p>Slutsumma: {simplifyValue(total,0)} kr (100%)</p>
-            <Row className="row-cols-auto">
-                {intrest.map((dataset) => paragraph(dataset, index, total))}
+        <Col>
+            <Row className="row justify-content-between">
+                <Col xl={9} className='col-xl'>
+                <h2>Resultat</h2>
+                    <p>Slutsumma: {simplifyValue(total,0)} kr (100%)</p>
+                    <Row className="row-cols-auto">
+                        {intrest.map((dataset) => paragraph(dataset, index, total))}
+                    </Row>
+                    <Row className="row-cols-auto">
+                        {acc.map((dataset) => paragraph(dataset, index, total))}
+                    </Row>
+                </Col>
+
+                <Col xl={3} className='col-xl'>
+                  {DoughnutChart(datasets, index, total)}
+                </Col>
             </Row>
-            <Row className="row-cols-auto">
-                {acc.map((dataset) => paragraph(dataset, index, total))}
-            </Row>
-        </div>
+        </Col>
     );
 }
-  
-  
 
   
