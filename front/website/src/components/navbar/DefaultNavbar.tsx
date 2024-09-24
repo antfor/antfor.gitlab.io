e0695fcee7195af3adb5c1833b6a53d56727905d
antfor <antfor@student.chalmers.se>
2024-09-24 13:30:54 +0200

    changed to correct extension .mts

--- front/website/src/components/navbar/DefaultNavbar.tsx
+++ front/website/src/components/navbar/DefaultNavbar.tsx
@@ -1,7 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {PAGES, page} from './Pages.js';
+import {PAGES, page} from './Pages.mts';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faGooglePlay } from '@fortawesome/free-brands-svg-icons'
 import './css/hamburger3.css';
