22f33d9e6747177eb0e255d852446bbf9ab49dd3
antfor <antfor@student.chalmers.se>
2024-10-07 17:32:13 +0200

    cv is loaded async

--- front/website/src/cv/pdf.mjs
+++ front/website/src/cv/pdf.mjs
@@ -1,8 +1,15 @@
 
-document.addEventListener("adobe_dc_view_sdk.ready", function(){ 
-    let adobeDCView = new AdobeDC.View({clientId: "c28f9f5736c045cd92ddd75062d1ecd6", divId: "adobe-dc-view"});
-    adobeDCView.previewFile({
-      content:{ location:{ url: "/Anton_Forsberg_CV.pdf"}},
-      metaData:{fileName: "Anton_Forsberg_CV.pdf"}
-    }, {embedMode: "IN_LINE", showPrintPDF: false});
-  });
+if(window.AdobeDC){
+  openPDF();
+}else{
+  document.addEventListener("adobe_dc_view_sdk.ready", openPDF);
+}
+
+
+async function openPDF(){
+  let adobeDCView = new AdobeDC.View({clientId: "c28f9f5736c045cd92ddd75062d1ecd6", divId: "adobe-dc-view"});
+  adobeDCView.previewFile({
+    content:{ location:{ url: "/Anton_Forsberg_CV.pdf"}},
+    metaData:{fileName: "Anton_Forsberg_CV.pdf"}
+  }, {embedMode: "IN_LINE", showPrintPDF: false});
+}
