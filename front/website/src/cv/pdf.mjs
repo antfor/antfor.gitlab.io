db6de9e2ed00741e6eba88d9e2c9298b662edc25
antfor <antfor@student.chalmers.se>
2024-10-07 02:14:12 +0200

    relative link to pdf

--- front/website/src/cv/pdf.mjs
+++ front/website/src/cv/pdf.mjs
@@ -2,7 +2,7 @@
 document.addEventListener("adobe_dc_view_sdk.ready", function(){ 
     let adobeDCView = new AdobeDC.View({clientId: "c28f9f5736c045cd92ddd75062d1ecd6", divId: "adobe-dc-view"});
     adobeDCView.previewFile({
-      content:{ location:{ url: "https://anton-forsberg.com/Anton_Forsberg_CV.pdf"}},
+      content:{ location:{ url: "/Anton_Forsberg_CV.pdf"}},
       metaData:{fileName: "Anton_Forsberg_CV.pdf"}
     }, {embedMode: "IN_LINE", showPrintPDF: false});
   });
