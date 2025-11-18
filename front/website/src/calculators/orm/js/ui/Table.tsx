27c84cd157749539587bd826f78d4dc0fe71ae16
antfor <38752136+antfor@users.noreply.github.com>
2025-11-18 23:40:08 +0100

    started on labels for Range input

--- front/website/src/calculators/orm/js/ui/Table.tsx
+++ front/website/src/calculators/orm/js/ui/Table.tsx
@@ -4,9 +4,11 @@ import {Result, PR, simplifyValue} from '../orm.mjs';
 
 
 export function TabelORM({data}:{data:Result}) {
-    const len =Math.min(data.reps.length ,Math.min(data.percantage.length, data.weight.length));
+
+  const len =Math.min(data.reps.length, Math.min(data.percantage.length, data.weight.length));
+
   return (
-    <Table striped bordered hover variant="dark">
+    <Table striped bordered hover>
       <thead>
         <tr>
           <th  >Percentage</th>
@@ -34,7 +36,9 @@ function colorDif(){
 }
 
 export function TabelPR({data}:{data:PR[]}) {
-    const len = data.length;
+
+  const len = data.length;
+  
   return (
     <Table striped bordered hover variant="dark">
       <thead>
@@ -53,41 +57,6 @@ export function TabelPR({data}:{data:PR[]}) {
                 <td>{simplifyValue(data[i].orm,1)}kg (+{simplifyValue(data[i].dif,2)}kg)</td>
             </tr>
         )}
-      </tbody>
-    </Table>
-  );
-}
-
-
-export function tabelB() {
-  return (
-    <Table striped bordered hover variant="dark">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
       </tbody>
     </Table>
   );
