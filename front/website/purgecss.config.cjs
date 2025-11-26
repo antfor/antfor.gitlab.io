109c93c93e47d644f099d6b0db9cebd46e303b17
antfor <38752136+antfor@users.noreply.github.com>
2025-11-26 14:25:19 +0100

    fixed purgeCSS

--- front/website/purgecss.config.cjs
+++ front/website/purgecss.config.cjs
@@ -3,6 +3,6 @@ module.exports = {
     css: ['./dist/assets/**/*.css'],
     safelist: {
         standard: ['html', 'body', 'btn'],
-        deep: [/^col/, /^navbar/, /^container/, /^btn/, /^gap/, /^dropdown/] 
+        deep: [/^col/, /^navbar/, /^container/, /^btn/, /^gap/, /^dropdown/, /^table/, /tooltip.*/, /feedback$/] 
     },
 };
